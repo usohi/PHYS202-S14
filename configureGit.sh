2b6bbf3703233751379c2eb56a7a7908abab8983
Umar Sohi <usohi@calpoly.edu>
2014-04-03 14:08:32 -0700

    Restoring repository

---
+++ configureGit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script configures git whenever you start working
+# on multiple machines.  Run it any time you decide to work 
+# with git on a new computer
+
+git config --global user.name "Umar Sohi"
+git config --global user.email "usohi@calpoly.edu"
+git config --global color.ui "auto"
+git config --global core.editor "nano"
